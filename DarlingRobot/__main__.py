897c0cbeb3f804ba56a86e989efe8bb9efe02e11
XHate-Official <100124630+XHate-Official@users.noreply.github.com>
2022-02-22 18:48:50 +0530

    Darling

--- DarlingRobot/__main__.py
+++ DarlingRobot/__main__.py
@@ -98,7 +98,7 @@ CHAT_SETTINGS = {}
 USER_SETTINGS = {}
 
 for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("SiestaRobot.modules." + module_name)
+    imported_module = importlib.import_module("DarlingRobot.modules." + module_name)
     if not hasattr(imported_module, "__mod_name__"):
         imported_module.__mod_name__ = imported_module.__name__
 
