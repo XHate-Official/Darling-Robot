c364c544f1a8fe9c12d97e2248964739751281d9
XDHate#Ded <98488953+XhateXd@users.noreply.github.com>
2022-03-05 20:18:37 +0530

    Darling

--- DarlingRobot/modules/memify.py
+++ DarlingRobot/modules/memify.py
@@ -37,9 +37,9 @@ async def drawText(image_path, text):
     shadowcolor = "black"
     i_width, i_height = img.size
     if os.name == "nt":
-        fnt = "ariel.ttf"
+        fnt = "Monterey.ttf"
     else:
-        fnt = "./DarlingRobot/resources/default.ttf"
+        fnt = "./DarlingRobot/resources/Monterey.ttf"
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
     if ";" in text:
         upper_text, lower_text = text.split(";")
