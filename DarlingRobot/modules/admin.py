9bb79b265a25a17cebadebaf85ae15148d74023f
XHate-Official <100124630+XHate-Official@users.noreply.github.com>
2022-03-07 15:12:18 +0530

    Darling

--- DarlingRobot/modules/admin.py
+++ DarlingRobot/modules/admin.py
@@ -891,7 +891,7 @@ dispatcher.add_handler(DEMOTE_HANDLER)
 dispatcher.add_handler(SET_TITLE_HANDLER)
 dispatcher.add_handler(ADMIN_REFRESH_HANDLER)
 
-__mod_name__ = "Admins"
+__mod_name__ = "✘ ᴀᴅᴍɪɴs  ✘"
 __command_list__ = [
     "setdesc"
     "setsticker"
