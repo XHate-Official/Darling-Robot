a474d7da5358998f30ab3c18abd373b76bff08f0
XDHate#Ded <98488953+XhateXd@users.noreply.github.com>
2022-03-07 17:01:47 +0530

    Update antiflood.py

--- DarlingRobot/modules/antiflood.py
+++ DarlingRobot/modules/antiflood.py
@@ -401,7 +401,7 @@ def __chat_settings__(chat_id, user_id):
 def helps(chat):
     return gs(chat, "antiflood_help")
 
-__mod_name__ = "Anti-Flood"
+__mod_name__ = "✘ ᴀɴᴛɪ-ғʟᴏᴏᴅ ✘"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.chat_type.groups,
