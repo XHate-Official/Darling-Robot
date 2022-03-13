203f01243ea51615dabb8fbf7d5f87e919fe3a41
XDHate#Ded <98488953+XhateXd@users.noreply.github.com>
2022-03-14 03:06:03 +0530

    Update myanimelist.py

--- DarlingRobot/modules/myanimelist.py
+++ DarlingRobot/modules/myanimelist.py
@@ -186,7 +186,9 @@ def manga(update: Update, context: CallbackContext):
         msg.reply_text(
             rep, parse_mode=ParseMode.HTML, reply_markup=InlineKeyboardMarkup(keyb)
         )
-
+        
+def helps(chat):
+        return gs(chat, "animelist_help")
 
 __mod_name__ = "MyAnimeList"
 
