49c8fd4947dbf4d333b853cd3de056f7e088dce0
XDHate#Ded <98488953+XhateXd@users.noreply.github.com>
2022-03-24 12:25:15 +0530

    Update userinfo.py

--- DarlingRobot/modules/userinfo.py
+++ DarlingRobot/modules/userinfo.py
@@ -469,8 +469,8 @@ def stats(update, context):
             status
             + "\n*Bot statistics*:\n"
             + "\n".join([mod.__stats__() for mod in STATS])
-            + f"\n\n[✘ Support ✘](@{SUPPORT_CHAT}) | [✘ Updates ✘](@{UPDATES_CHANNEL})\n\n"
-            + "\n╘══「 By [× ʜᴀᴛᴇ ×](@x_hate) 」\n",
+            + f"\n\n[✘ Support ✘](https://t.me/ZeroTwoXSupport) | [✘ Updates ✘](https://t.me/zerotwoxupdates)\n\n"
+            + "\n╘══「 By [× ʜᴀᴛᴇ ×](https://t.me/x_hate) 」\n",
             parse_mode=ParseMode.MARKDOWN,
         )
     except BaseException:
