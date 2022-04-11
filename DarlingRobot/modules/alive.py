5bcde5cb7290bbb381e774f9b7f802a4c2e6160c
KarlexD <98577270+KarlexD@users.noreply.github.com>
2022-04-11 12:32:58 +0530

    Update alive.py

--- DarlingRobot/modules/alive.py
+++ DarlingRobot/modules/alive.py
@@ -13,11 +13,11 @@ PHOTO = "https://telegra.ph/file/44d61a5c0844af1f03239.mp4"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**⊱ ───── ஓ๑♡๑ஓ ───── ⊰**\n"
+  TEXT = f"**⊱ ───── ஓ๑✯๑ஓ ───── ⊰**\n"
   TEXT +=  f"**ʏᴏ [{event.sender.first_name}](tg://user?id={event.sender.id}), ᴍʏsᴇʟғ ᴢᴇʀᴏ ᴛᴡᴏ.** \n"
   TEXT += "☮︎ **ᴋɪᴅᴅᴏ, ɪ'ᴍ ᴀʟɪᴠᴇ** \n"
   TEXT += f"☮︎ **ᴍʏ ᴍᴀsᴛᴇʀ : [|×✠×|](https://t.me/xD_Zent)** \n"
   TEXT += "**×͜× ᴛʜᴀɴᴋs ғᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ ʜᴇʀᴇ\n**"
-  TEXT += "**⊱ ───── ஓ๑♡๑ஓ ───── ⊰**"
+  TEXT += "**⊱ ───── ஓ๑✯๑ஓ ───── ⊰**"
   BUTTON = [[Button.url("✘ ʜᴇʟᴘ ✘", "https://t.me/Foundingtitanbot?start=help"), Button.url("✘ sᴜᴘᴘᴏʀᴛ ✘", "https://t.me/AckermanXClan")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
