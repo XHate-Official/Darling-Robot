8c810ef610c9f4cc8767205995827df597eb71e6
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-04-12 11:35:40 +0530

    Update config.py

--- DarlingRobot/config.py
+++ DarlingRobot/config.py
@@ -70,6 +70,7 @@ class Config(object):
     AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
+    KUKI_API = "" # KUKI API TOKEN TO GET @KUKICHATBOT
     REM_BG_API_KEY = "xYCR1ZyK3ZsofjH7Y6hPcyzC"
     OPENWEATHERMAP_ID = "887da2c60d9f13fe78b0f9d0c5cbaade"
 
